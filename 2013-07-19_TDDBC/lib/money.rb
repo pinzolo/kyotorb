4d3a74ef17a8f5a1048a3dbe1725ed9a4becad16
pinzlo <pinzolo@gmail.com>
2013-07-19 11:43:52 +0900

    implements money class

---
+++ 2013-07-19_TDDBC/lib/money.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+class Money
+  attr_accessor :value
+  def initialize(value)
+    @value = value
+  end
+
+  class << Money
+    def one
+      Money.new(1)
+    end
+
+    def five
+      Money.new(5)
+    end
+
+    def ten
+      Money.new(10)
+    end
+
+    def fifty
+      Money.new(50)
+    end
+
+    def hundred
+      Money.new(100)
+    end
+
+    def five_hundred
+      Money.new(500)
+    end
+
+    def thousand
+      Money.new(1000)
+    end
+
+    def five_thousand
+      Money.new(5000)
+    end
+
+    def ten_thousand
+      Money.new(10000)
+    end
+  end
+end
